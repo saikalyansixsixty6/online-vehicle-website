e92cf1fcf8f00d624e9a9697bd61cc5e44faad94
Saikalyan <saikalyan2406@gmail.com>
2023-12-13 13:56:20 +0530

    my context

---
+++ frontend/src/context/MyState.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import MyContext from './MyContext'
+
+
+const MyState = (props) => {
+
+
+
+
+
+
+
+
+
+
+    
+  return (
+    <MyContext.Provider value={{}}>
+      {props.children}
+    </MyContext.Provider>
+  )
+}
+
+export default MyState
