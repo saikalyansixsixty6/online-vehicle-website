fe0ce585a7bb1da4c13df4c3f2c7f362105846a8
Taterao Jinkalwar <122861107+Taterao3133@users.noreply.github.com>
2023-12-15 14:06:10 +0530

    now close chat page added to request-drive

--- frontend/src/pages/Chatpopup.jsx
+++ frontend/src/pages/Chatpopup.jsx
@@ -1,21 +1,31 @@
 import React, { useState } from 'react'
 import './Chatpopup.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import PropTypes from 'prop-types';
 
-function Chatpopup() {
-    
-  
+import { faTimes, } from '@fortawesome/free-solid-svg-icons';
+
+
+
+function Chatpopup({ onClose }) {
+
+
     return (
-        <div className='chat-container'>
-             <div className="chat-popup">
-                
-                <div className="chat-content">
-                
-                    <p>Welcome to the chat!</p>
-                </div>
+      <div className='chat-container'>
+        <div className="chat-popup">
+          <div className="chat-content">
+            <p>Welcome to the chat!</p>
+            <div className="chat-close-icon">
+              <FontAwesomeIcon onClick={onClose} icon={faTimes} className='close-icon relative w-9 h-9 ml-80 -mt-7 cursor-pointer' />
             </div>
+          </div>
         </div>
-  );
+      </div>
+    );
   }
   
-
-export default Chatpopup
+  Chatpopup.propTypes = {
+    closeChat: PropTypes.func.isRequired,
+  };
+  
+  export default Chatpopup;
--- frontend/src/shared/Carcard.jsx
+++ frontend/src/shared/Carcard.jsx
@@ -2,16 +2,23 @@
 import {Card, CardBody} from 'reactstrap';
 import '../shared/carcard.css'
 import { Link } from 'react-router-dom';
-// import ChatPopup from '../pages/Chatpopup';
-// import { useState } from 'react';
-
+import ChatPopup from '../pages/Chatpopup';
+import { useState } from 'react';
+import PropTypes from 'prop-types';
 
 const CarCard = ({car}) => {
     const {_id, BrandName,photo,price }=car;
-    
+    const [isChatOpen, setChatOpen] = useState(false);
 
-
+    const openChat = () => {
+      console.log('Opening chat...');
+      setChatOpen(true);
+    };
     
+    const closeChat = () => {
+      console.log('Closing chat...');
+      setChatOpen(false);
+    };
 
   return <>
     <div className="car_container">
@@ -23,11 +30,13 @@ const CarCard = ({car}) => {
         <CardBody className="car_info">
           <p className="brand text-lefd">{BrandName}</p>
           <p>{price}</p>
-          <Link to='/chatpopup'><button>Request to Test Drive</button> </Link>
-          
-          
+          <button onClick={openChat}>Request to Test Drive</button> 
+          {isChatOpen && <ChatPopup onClose={closeChat} />}  
         </CardBody>
       </div>
     </>
 }
+CarCard.propTypes = {
+  car: PropTypes.object.isRequired,
+};
 export default CarCard;
